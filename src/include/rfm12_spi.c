c049f2fff35443a516ebb4eb2d6a4e8e3748d72a
James Boydell <boydell.james@gmail.com>
2015-03-16 21:29:05 -0400

    Made changed to work with ATMEGA324 and atmel studio 6.2

--- src/include/rfm12_spi.c
+++ src/include/rfm12_spi.c
@@ -59,11 +59,11 @@ static uint8_t spi_data(uint8_t c) {
 static void __attribute__ ((noinline)) rfm12_data(uint16_t d) {
 	SS_ASSERT();
 	#if !(RFM12_SPI_SOFTWARE)
-		SPDR = d >> 8;
-		while (!(SPSR & (1<<SPIF)));
+		SPDR0 = d >> 8;
+		while (!(SPSR0 & (1<<SPIF0)));
 
-		SPDR = d & 0xff;
-		while (!(SPSR & (1<<SPIF)));
+		SPDR0 = d & 0xff;
+		while (!(SPSR0 & (1<<SPIF0)));
 	#else
 		spi_data(d >> 8);
 		spi_data(d & 0xff);
@@ -79,12 +79,12 @@ static uint16_t __attribute__ ((noinline)) rfm12_read(uint16_t c) {
 	SS_ASSERT();
 
 	#if !(RFM12_SPI_SOFTWARE)
-		SPDR = c >> 8;
-		while (!(SPSR & (1<<SPIF)));
-		retval = SPDR << 8;
-		SPDR = c & 0xff;
-		while (!(SPSR & (1<<SPIF)));
-		retval |= SPDR;
+		SPDR0 = c >> 8;
+		while (!(SPSR0 & (1<<SPIF0)));
+		retval = SPDR0 << 8;
+		SPDR0 = c & 0xff;
+		while (!(SPSR0 & (1<<SPIF0)));
+		retval |= SPDR0;
 	#else
 		retval = spi_data(c >> 8);
 		retval <<= 8;
@@ -101,10 +101,10 @@ static uint16_t __attribute__ ((noinline)) rfm12_read(uint16_t c) {
 static uint8_t rfm12_read_int_flags_inline(void) {
 	#if !(RFM12_SPI_SOFTWARE)
 		SS_ASSERT();
-		SPDR = 0;
-		while (!(SPSR & (1<<SPIF)));
+		SPDR0 = 0;
+		while (!(SPSR0 & (1<<SPIF0)));
 		SS_RELEASE();
-		return SPDR;
+		return SPDR0;
 	#else
 		SS_ASSERT();
 		unsigned char x, d = d;
@@ -133,7 +133,7 @@ static void spi_init(void) {
 	DDR_MISO &= ~(_BV(BIT_MISO));
 
 	#if !(RFM12_SPI_SOFTWARE)
-		SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); //SPI Master, clk/16
+		SPCR0 = (1<<SPE0) | (1<<MSTR0) | (1<<SPR00); //SPI Master, clk/16
 	#endif
 }
 
